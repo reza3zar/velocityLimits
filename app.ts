435ceb3ebe03d6008814e89d90c48dd55bd36f3d
Reza Younesi <younesi.nima@gmail.com>
2021-02-16 09:49:23 -0500

    Update app.ts

--- app.ts
+++ app.ts
@@ -13,30 +13,41 @@ import { CustomerTransaction } from './models/tree-transaction_viewmodel/custome
  
 let velocityLimitsEvaluator=()=> {
   try {
+
+    // Load input file from data folder. 
     let transactionRequest: Array<TransactionRequest> = transactionRequestSource;
+    // Check input file has value
     const isValidDataSrc = ValidatorDataSrc.validateDataSource(transactionRequest);
-  
+    // If input file does not have any data, throw exception data not found.
     if (!isValidDataSrc)
       throw new Error(errorMessages.errors['input-not-found']);
-  
+    // Convert input file (transactions) to CustomerTransaction model 
+    // CustomerTransaction is a tree for specific customer than it contains all cusomer's transactions.
     const customerTransactionTreeCollection: Array<CustomerTransaction> = transactionTreeHandler.CustomerTransactionHandler
       .createCustomerTransactionTree(transactionRequest);
   
+    // Check customerTransactionTreeCollection has any value, if it doesn't throw exception
     if (!customerTransactionTreeCollection || customerTransactionTreeCollection.length === 0)
       throw new Error(errorMessages.errors['transaction-request-collection-not-found']);
   
+    /* Evaluator is a core that should check customerTransaction
+    per customer that can be accepted or not base velocity limits (velocityLimits are in setting folder)*/
     const transactionResponseCollection: Array<TransactionResponse> =
       evals.TransactionEvaluator.transactionEval(customerTransactionTreeCollection);
   
+    // check transactionResponse has any value, if it doesn't throw exception
     if (!transactionResponseCollection || transactionResponseCollection.length === 0)
       throw new Error(errorMessages.errors['transaction-response-collection-not-found']);
-    
+    // print transactionResponse (output) in console.    
       transactionResponseCollection.forEach(trnsResponse=>console.log(trnsResponse))
   } catch (error) {
     console.error(error);
   }  
 }
-   
+
+
+console.log('------------------------------------------START --------------------------------------------------------')
+
 velocityLimitsEvaluator();
  
 
